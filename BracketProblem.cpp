507ade21428aa58083842c1fd141df88887a65af
Waleed <p.e.k.k.a21000@gmail.com>
2024-07-30 12:26:03 +0500

    Two conditions done

--- BracketProblem.cpp
+++ BracketProblem.cpp
@@ -1,15 +1,44 @@
 #include <iostream>
+#include <string>
+#include <conio.h>
 using namespace std;
 main(){
+    bool checkCondition2(int length);
+    bool checkCondition1(string array, int length);
     string array;
     cout << "Enter a string: ";
     getline(cin, array);
     int length = array.length();
     bool condition1;
-    condition1 = checkEvenNumber(length);
-    
+    bool condition2;
+    cout << length;
+    getch();
+    condition1 = checkCondition1(array, length);
+    condition2 = checkCondition2(length);
+
 }
-bool checkEvenNumber(int length)
+bool checkCondition2(int length)
 {
-    if (length%2!=0 ? true : false)
+    if (length%2!=0)
+    {
+        return false;
+    }
+    else 
+    {
+        return true;
+    }
+}
+bool checkCondition1(string array, int length)
+{
+    for (int i =0;i<length;i++)
+    {
+        if (array[i] == '(' || array[i] == ')' || array[i] == '{' || array[i] == '}' || array[i] == '[' || array[i] == ']')
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 }
